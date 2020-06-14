9cd646bca2dce252757b0c2c638d7190f26b0845
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-06-14 19:32:07 +0530

    Connected to Database

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
+mongoose.connect("mongodb://localhost/touristSpots");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
 
-let tspots = [
-  {
-    name: "Patna Museum",
-    imageURL:
-      "https://4.bp.blogspot.com/-tM6rL9-Uixk/Vth3RRpaghI/AAAAAAAAA2w/TXdmfCvbePg/s1600/patna-museum-hd-wallpaper-image.jpg",
-  },
-  {
-    name: "Gandhi Maidan",
-    imageURL:
-      "https://upload.wikimedia.org/wikipedia/en/b/b4/Gandhi_Maidan.jpg",
-  },
-  {
-    name: "Patna Zoo",
-    imageURL:
-      "https://www.travelbaits.in/wp-content/uploads/2019/08/Travelbaits_Patna-Zoo-600x450.jpeg?p=2318",
-  },
-  {
-    name: "Energy Park",
-    imageURL:
-      "https://scontent.fccu17-1.fna.fbcdn.net/v/t1.0-9/16806802_1852573335032354_7931790811273432280_n.jpg?_nc_cat=107&_nc_sid=09cbfe&_nc_ohc=Wr_I9aEJkHsAX9mvbhA&_nc_ht=scontent.fccu17-1.fna&oh=ed00dc65445cfcd2007a9f145a9d9a1e&oe=5F09359A",
-  },
-];
+const tspotSchema = new mongoose.Schema({
+  name: String,
+  imageURL: String,
+});
+
+const tspot = mongoose.model("tspot", tspotSchema);
 
 app.get("/", (req, res) => {
   res.render("landing");
 });
 
 app.get("/touristspots", (req, res) => {
-  res.render("touristspots", { tspots: tspots });
+  tspot.find({}, (err, allTSpots) => {
+    if (err) {
+      console.log("Some error occured while traversing database!");
+    } else {
+      res.render("touristspots", { tspots: allTSpots });
+    }
+  });
 });
 
 app.post("/touristspots", (req, res) => {
   let name = req.body.name;
   let imageURL = req.body.imageURL;
   let newSpot = { name: name, imageURL: imageURL };
-  tspots.push(newSpot);
+
+  tspot.create(newSpot, (err, newTSpot) => {
+    if (err) {
+      console.log("Some error occured while adding newTSpot to database!");
+    } else {
+      console.log("New Tourist Spot saved to database.");
+      console.log(newTSpot);
+    }
+  });
   res.redirect("/touristspots");
 });
 
---
+++ public/myjavascript.js
@@ -0,0 +1,7 @@
+$(".navbar a").click(function (event) {
+  // check if window is small enough so dropdown is created
+  var toggle = $(".navbar-toggler").is(":visible");
+  if (toggle) {
+    $(".navbar-collapse").collapse("hide");
+  }
+});
