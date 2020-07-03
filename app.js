2ab3f5e63c12187b08e1f30412fe9190b808e946
Ashutosh Anand <ashutoshanand139@gmail.com>
2020-07-03 20:55:29 +0530

    Added Cloud MongoDB

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ const commentRoutes = require("./routes/comments");
 const indexRoutes = require("./routes/index");
 app.locals.moment = require("moment");
 
-mongoose.connect("mongodb://localhost/touristSpots", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongodb://localhost/touristSpots
+mongoose.connect(
+  "mongodb+srv://Ashutosh:securepassword@exploration-point.1kpnb.mongodb.net/test",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
